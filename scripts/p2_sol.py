9f21470b3807927a75a4e7f62803fb2f7b8b9ed1
charliecaputo <c3caputo@gmail.com>
2024-02-07 23:39:20 +0000

    finished_not final

--- scripts/p2_sol.py
+++ scripts/p2_sol.py
@@ -2,46 +2,57 @@
 import math
 import numpy as np
 
-if __name__ == '__main__':
-	#test values
-	tx = 2	# x trans value
-	ty = 3	# y trans value
-	tz = 4	# z trans value
-	psi = math.pi/2		#x rot value
-	theta = math.pi/2	#y rot value
-	phi = math.pi/2		#z rot value
-	
-	#update output format
-	np.set_printoptions(precision = 2, suppress= True)
-	
-	#define 4d vector from example
-	v0 = np.array([0,1,1,1]) #start coords
-	
-	#translation matrices and print each one 
-	T = np.array([[1,0,0,tx],
-				  [0,1,0,ty],
-				  [0,0,1,tz],
-				  [0,0,0,1]])
-	print("Translation matrix that represents (x,y,z) as (2,3,4) respectively: \n", T)
-	
-	
-	#rotation for x axis and print matrix
-	Rx = np.array([[1, 0, 0, 0], 
-	               [0, np.cos(psi), -np.sin(psi), 0],
-	               [0, np.sin(psi), np.cos(psi), 0], 
-	               [0, 0, 0, 1]])
-	print("rotation matrix for basic homogenous transformation about x axis:\n ", Rx)
-	
-	#rotation for y axis and print matrix
-	Ry = np.array([[np.cos(theta), 0, np.sin(theta), 0], 
-	               [0,1,0,0], 
-	               [-np.sin(theta), 0, np.cos(theta), 0], 
-	               [0,0,0,1]])
-	print("rotation matrix for basic homogenous transformation about x axis:\n ", Ry)
-	
-	#rotation for z axis and print matrix
-	Rz = np.array([[np.cos(phi), -np.sin(phi),0,0], 
-	               [np.sin(phi), np.cos(phi), 0, 0], 
-	               [0,0,1,0], 
-	               [0,0,0,1]])
-	print("rotation matrix for basic homogenous transformation about x axis:\n ", Rz)
+
+#test values
+tx = 2	# x trans value
+ty = 3	# y trans value
+tz = 4	# z trans value
+psi = math.pi/2		#x rot value
+theta = math.pi/2	#y rot value
+phi = math.pi/2		#z rot value
+
+#update output format
+np.set_printoptions(precision = 2, suppress= True)
+
+#define 4d vector from example
+v0 = np.array([0,1,1,1]) #start coords
+
+#create translation matrices
+def Tx(x):
+	return np.array([[1,0,0,x],
+				    [0,1,0,0],
+				    [0,0,1,0],
+				    [0,0,0,1]])
+def Ty (y): 
+	return np.array([[1,0,0,0],
+				  	[0,1,0,ty],
+				  	[0,0,1,0],
+				  	[0,0,0,1]])
+def Tz (z):
+	return np.array([[1,0,0,0],
+				  	[0,1,0,0],
+				  	[0,0,1,tz],
+				  	[0,0,0,1]])
+
+
+#rotation for x axis and print matrix
+def Rx (psi):
+	return np.array([[1, 0, 0, 0], 
+	                 [0, np.cos(psi), -np.sin(psi), 0],
+	            	 [0, np.sin(psi), np.cos(psi), 0], 
+	            	 [0, 0, 0, 1]])
+
+#rotation for y axis and print matrix
+def Ry (theta):
+	return np.array([[np.cos(theta), 0, np.sin(theta), 0], 
+	            [0,1,0,0], 
+	            [-np.sin(theta), 0, np.cos(theta), 0], 
+	            [0,0,0,1]])
+
+#rotation for z axis and print matrix
+def Rz (phi):
+	return np.array([[np.cos(phi), -np.sin(phi),0,0], 
+	            [np.sin(phi), np.cos(phi), 0, 0], 
+	            [0,0,1,0], 
+	            [0,0,0,1]])
+
--- scripts/p3_sol.py
+++ scripts/p3_sol.py
@@ -1,10 +1,42 @@
 #import stuff including p2_solve.py
-import p2_solve as p2
+import p2_sol as p2
 import math
 import numpy as np
  
 if __name__ == '__main__':
-	#transformation 1 
-	T1_x = p2.translation_matrix(2.5,0,0)
-	T2_y = p2.translation_matrix(0,-1.5,0)
-	t3_z = p2.translation_matrix(0,0,0.5)
+
+	#define the fixed vector/ origin
+	v0 = np.array([[1,0,0,0],
+					[0,1,0,0],
+					[0,0,1,0],
+					[0,0,0,1]])
+					
+	current_pos = np.array([[1,0,0,0],
+					 		[0,1,0,0],
+							[0,0,1,0],
+							[0,0,0,1]])
+	
+	#transformation 1
+	H_1 = np.dot(p2.Tx(2.5), np.dot(p2.Tz(0.5), p2.Ty(-1.5)))
+	result_1 = current_pos = np.dot(H_1, current_pos)
+	print("Transformation 1: \n", result_1)
+	
+	#transformation 2
+	H_2 = np.dot(p2.Tz(0.5), np.dot(p2.Tx(2.5), p2.Ty(-1.5)))
+	result_2 = np.dot(H_2, current_pos) #ask if i should dot by H_1 since its current and not fixed
+	print("Transformation 2: \n", result_2)
+	
+	#transformation 3 this is the same as one because there is no rotation
+	H_3 = np.dot(p2.Tx(2.5), np.dot(p2.Tz(0.5), p2.Ty(-1.5))) 
+	print("Transformation 3: \n", H_3)
+	
+	#transformation 4 same as two because there is no rotation and both are starting at origin
+	H_4 = np.dot(p2.Tz(0.5), np.dot(p2.Tx(2.5), p2.Ty(-1.5)))
+	print("Transformation 3: \n", H_4)
+	
+	#Transformation 5
+	H_5 = np.dot(p2.Rx(np.pi/2), np.dot(p2.Tx(3), np.dot(p2.Tz(-3), p2.Rz(-np.pi/2))))
+	result_5 = np.dot(H_5, current_pos)
+	print("transformation 5: \n", result_5)
+	
+	
