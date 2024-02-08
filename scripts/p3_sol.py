451c98dde442cd5a178e6a504ca900f1be9e8634
charliecaputo <c3caputo@gmail.com>
2024-02-08 17:18:05 +0000

    Final_Update

--- scripts/p3_sol.py
+++ scripts/p3_sol.py
@@ -23,22 +23,22 @@ if __name__ == '__main__':
 	
 	#transformation 2
 	H_2 = np.matmul(p2.Tz(0.5), np.matmul(p2.Tx(2.5), p2.Ty(-1.5)))
-	result_2 = np.dot(H_2, current_pos)
+	result_2 = current_pos = np.dot(H_2, current_pos)
 	print("Transformation 2: \n", result_2)
 	
 	#transformation 3
-	H_3 = np.matmul(p2.Tx(2.5), np.matmul(p2.Tz(0.5), p2.Ty(-1.5))) 
-	result_3 = np.dot(H_3, Fixed_Frame)
+	H_3 = np.matmul(p2.Tx(2.5), np.matmul(p2.Ty(-1.5), p2.Tz(0.5))) 
+	result_3 = current_pos = np.dot(H_3, Fixed_Frame)
 	print("Transformation 3: \n", H_3)
 	
 	#transformation 4 
-	H_4 = np.matmul(p2.Tz(0.5), np.matmul(p2.Tx(2.5), p2.Ty(-1.5)))
-	result_4 = np.dot(H_3, Fixed_Frame)
+	H_4 = np.matmul(p2.Tz(0.5), np.matmul(p2.Ty(-1.5),p2.Tx(2.5)))
+	result_4 = current_pos = np.dot(H_3, Fixed_Frame)
 	print("Transformation 3: \n", H_4)
 	
 	#Transformation 5
 	H_5 = np.matmul(p2.Rx(np.pi/2), np.matmul(p2.Tx(3), np.matmul(p2.Tz(-3), p2.Rz(-np.pi/2))))
-	result_5 = np.dot(H_5, current_pos)
+	result_5 = current_pos = np.dot(H_5, current_pos)
 	print("transformation 5: \n", result_5)
 	
 	
